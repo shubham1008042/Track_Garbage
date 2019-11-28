efc312946bb4dadc496ce6922693f45eecc52c33
Shubham Sharma <ss1008042@gmail.com>
2019-11-28 17:50:38 +0530

    Add Accounts app Folder

--- SGM/accounts/forms.py
+++ SGM/accounts/forms.py
@@ -1,25 +1,14 @@
-from django import forms
-from django.contrib.auth.models import User
-# from django.contrib.auth import get_user_model
-# User = get_user_model()
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
 
+class CustomUserCreationForm(UserCreationForm):
 
-class SignupForm(forms.Form):
-    email = forms.EmailField(max_length=200, help_text='Required')
-    password = forms.CharField(min_length=8, widget=forms.PasswordInput(), required=True)
-    first_name= forms.CharField(max_length=100, help_text='Required')
-    last_name= forms.CharField(max_length=100, help_text='Required')
-    
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email','first_name','last_name')
+
+class CustomUserChangeForm(UserChangeForm):
+
     class Meta:
-        model = User
-        fields = ('email', 'first_name', 'last_name','password')
-    
-    
-    def save(self):
-            data = self.cleaned_data
-            user = User(
-                email=data['email'],
-                is_active=False,
-                password=make_password(data['password'])
-            )
-            return user.save()
+        model = CustomUser
+        fields = ('username', 'email','first_name','last_name')
--- SGM/accounts/urls.py
+++ SGM/accounts/urls.py
@@ -1,7 +1,25 @@
+# from django.urls import path,re_path
+# from accounts import views as account_views
+# from django.views.generic import TemplateView
+
+# urlpatterns = [
+#     path('signup/', views.SignUp.as_view(), name='signup'),
+#     path('accounts/login/',accounts_views.login,name='login'),
+#     path('accounts/logout/', accounts_views.logout,name='logout'),
+#     path('accounts/password_change/',accounts_views.password_change,name='password_change'),
+#     path('accounts/password_change/done/', accounts_views.password_change_done,name='password_change_done'),
+#     path('accounts/password_reset/',accounts_views.password_reset,name='password_reset'),
+#     path('accounts/password_reset/done/',accounts_views.password_reset_done,name='password_reset_done'),
+#     re_path('accounts/reset/<uidb64>/<token>/',accounts_views.password_reset_confirm,name='password_reset_confirm'),
+#     path('accounts/reset/done/', accounts_views.password_reset_complete,name='password_reset_complete'),
+
+# ]
+
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('signup/', views.Signup.as_view(), name='signup'),
-    path('activate/<str:uid>/<str:token>', views.Activate.as_view(), name='activate'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+	path('profile/', TemplateView.as_view(template_name='profile.html'), name='profile'),
 ]
