ae5250e3e3c9ed05e41e37137ce4efd004bdda0f
Shubham Sharma <ss1008042@gmail.com>
2019-12-04 18:38:07 +0530

    changes in accounts template

--- SGM/accounts/forms.py
+++ SGM/accounts/forms.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = ('username', 'email','first_name','last_name')
+        model =User
+        fields = ( 'email','first_name','last_name')
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
-        model = CustomUser
-        fields = ('username', 'email','first_name','last_name')
+        model = User
+        fields = ('email','first_name','last_name')
--- SGM/accounts/views.py
+++ SGM/accounts/views.py
@@ -8,4 +8,4 @@ from .forms import CustomUserCreationForm
 class SignUp(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/login.html'
